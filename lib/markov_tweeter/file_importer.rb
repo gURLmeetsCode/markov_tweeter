cc232638386d62fc9f9c302213e77534c7fd5f77
Natasha Kelly <kellynatashav@yahoo.com>
2017-10-01 16:28:08 -0400

    fix methods in file importer

--- lib/markov_tweeter/file_importer.rb
+++ lib/markov_tweeter/file_importer.rb
@@ -1,15 +1,31 @@
 class MarkovTweeter::FileImporter
-  attr_reader :graph, :order
+  attr_reader :graph, :order, :files
 
-  def train
+  def initialize
+    @files = []
+  end
+
+  def read
     File.open(File.dirname(__FILE__) + '/tweets.txt', "r") do |f|
       f.each_line do |line|
-        puts "#{line}"
+        @files ||= line
       end
     end
   end
 
 
+def train
+  i = 0
+  while i < @files.length - @order do
+    gram = @files.slice(i, i + @order)
+    if @graph.include? gram
+      @graph[gram] = []
+    else
+      @graph[gram].push(@files.slice(i + @order))
+    end
+    i += 1
+  end
+end
 
 
 end
