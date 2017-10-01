1e1feba9ab20a613dacbf79759f1a16822f43c67
Natasha Kelly <kellynatashav@yahoo.com>
2017-10-01 16:16:04 -0400

    fix errors in file importer

--- lib/markov_tweeter/file_importer.rb
+++ lib/markov_tweeter/file_importer.rb
@@ -4,16 +4,7 @@ class MarkovTweeter::FileImporter
   def train
     File.open(File.dirname(__FILE__) + '/tweets.txt', "r") do |f|
       f.each_line do |line|
-        i = 0
-        while i < line.length - @order do
-          gram = line.slice(i, i + @order)
-          if @graph.include? gram
-            @graph[gram] = []
-          else
-            @graph[gram].push(line.slice(i + @order))
-          end
-          i += 1
-        end
+        puts "#{line}"
       end
     end
   end
